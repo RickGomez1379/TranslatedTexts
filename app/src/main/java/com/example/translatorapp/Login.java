32447a7579f8856d6bb11a0a5fb23799b8eea527
Ricardo Gomez <RGomez2@student.fullsail.edu>
2023-02-06 19:20:48 -0500

    Added Firebase Login Function HandleLogin() Renamed Objects in login and register xml

--- app/src/main/java/com/example/translatorapp/Login.java
+++ app/src/main/java/com/example/translatorapp/Login.java
@@ -1,14 +1,47 @@
 package com.example.translatorapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Login extends AppCompatActivity {
 
+    private Button login;
+    private TextView registerInstead;
+    private EditText email;
+    private EditText password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        login = findViewById(R.id.LoginButton);
+        registerInstead = findViewById(R.id.RegisterHere);
+        email = findViewById(R.id.LoginEmail);
+        password = findViewById(R.id.LoginPassword);
+
+    }
+
+    //Login Function
+    private void HandleLogin()
+    {
+        FirebaseAuth.getInstance().signInWithEmailAndPassword(email.getText().toString(), password.getText().toString()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful())
+                    Toast.makeText(Login.this, "Logged in Successfully", Toast.LENGTH_SHORT).show();
+                else
+                    Toast.makeText(Login.this, task.getException().getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
