cc8eb7f9a2208f7c746a31aee140666c33a61674
Rick <rickgomez1379@gmail.com>
2023-02-14 19:43:28 -0500

    Added Log out Functionality

--- app/src/main/java/com/example/translatorapp/Profile.java
+++ app/src/main/java/com/example/translatorapp/Profile.java
@@ -2,13 +2,31 @@ package com.example.translatorapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Profile extends AppCompatActivity {
+
+    private Button logOutBtn;
+    private Button uploadBtn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        logOutBtn = findViewById(R.id.logOut);
+        logOutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(Profile.this, Login.class));
+                finish();
+            }
+        });
     }
 }
