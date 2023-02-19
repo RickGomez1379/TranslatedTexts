f3d704531401167e9e18039bc257a74b77ee8b4e
Rick <rickgomez1379@gmail.com>
2023-02-18 22:21:05 -0500

    Added Language Spinner in Register Activity(Used for Translate Message)

--- app/src/main/java/com/example/translatorapp/Register.java
+++ app/src/main/java/com/example/translatorapp/Register.java
@@ -6,8 +6,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -16,6 +19,7 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.ml.naturallanguage.translate.FirebaseTranslateLanguage;
 
 public class Register extends AppCompatActivity {
 
@@ -25,7 +29,10 @@ public class Register extends AppCompatActivity {
     private EditText password;
     private EditText reenterPassword;
     private EditText username;
-
+    private Spinner languageSpinner;
+    String[] Languages = { "English", "Afrikaans", "Arabic", "Belarusian", "Bulgarian",
+            "Bengali", "Catalan", "Czech", "Welsh", "Hindi", "Urdu"};
+    private int languageCode;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,6 +43,9 @@ public class Register extends AppCompatActivity {
         password = findViewById(R.id.RegisterPassword);
         reenterPassword = findViewById(R.id.ReenterPassword);
         username = findViewById(R.id.RegisterUsername);
+        languageSpinner = findViewById(R.id.languageSpinner);
+
+        //
         register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -56,6 +66,21 @@ public class Register extends AppCompatActivity {
                 finish();
             }
         });
+        languageSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int position, long id) {
+                languageCode = getLanguageCode(Languages[position]);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+        //Adapter to display translates "from" languages
+        ArrayAdapter fromAdapter = new ArrayAdapter(this,R.layout.spinner_item,Languages);
+        fromAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        languageSpinner.setAdapter(fromAdapter);
 
     }
 
@@ -65,7 +90,8 @@ public class Register extends AppCompatActivity {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
                 if(task.isSuccessful()) {
-                    FirebaseDatabase.getInstance().getReference("user/" + FirebaseAuth.getInstance().getCurrentUser().getUid()).setValue(new User(email.getText().toString(), "", username.getText().toString()));
+                    FirebaseDatabase.getInstance().getReference("user/" + FirebaseAuth.getInstance().getCurrentUser().getUid())
+                            .setValue(new User(email.getText().toString(), "", username.getText().toString()));
                     Toast.makeText(Register.this, "Signed Up Successfully", Toast.LENGTH_SHORT).show();
                     startActivity(new Intent(Register.this, AllMessages.class));
                     finish();
@@ -74,5 +100,47 @@ public class Register extends AppCompatActivity {
                     Toast.makeText(Register.this, task.getException().getLocalizedMessage(), Toast.LENGTH_SHORT).show();
             }
         });
+    }
+    private int getLanguageCode(String fromLanguage) {
+        int languageCode = -1;
+        switch (fromLanguage) {
+            case "English":
+                languageCode = FirebaseTranslateLanguage.EN;
+                break;
+            case "Afrikaans":
+                languageCode = FirebaseTranslateLanguage.AF;
+                break;
+            case "Arabic":
+                languageCode = FirebaseTranslateLanguage.AR;
+                break;
+            case "Belarusian":
+                languageCode = FirebaseTranslateLanguage.BE;
+                break;
+            case "Bulgarian":
+                languageCode = FirebaseTranslateLanguage.BG;
+                break;
+            case "Bengali":
+                languageCode = FirebaseTranslateLanguage.BN;
+                break;
+            case "Catalan":
+                languageCode = FirebaseTranslateLanguage.CA;
+                break;
+            case "Czech":
+                languageCode = FirebaseTranslateLanguage.CS;
+                break;
+            case "Welsh":
+                languageCode = FirebaseTranslateLanguage.CY;
+                break;
+            case "Hindi":
+                languageCode = FirebaseTranslateLanguage.HI;
+                break;
+            case "Urdu":
+                languageCode = FirebaseTranslateLanguage.UR;
+                break;
+            default:
+                languageCode = 0;
+                break;
+        }
+        return languageCode;
     }
 }
